4a72c1251c121b8a1617485e0d041a91e84cb5d3
Francisco <fnaveira11@gmail.com>
2021-03-07 13:13:05 -0500

    Added additional content to script, and aligned headers

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+const p1 = document.createElement('p1');
+p1.textContent = 'The primary purpose of this website is to give myself (Francisco) a sand box to practice JavaScript, CSS, and HTML. I may also add some personal information about myself too, but do not expect anything too fancy yet. I will continue to add to this web page as I learn more about coding. ';
+document.querySelector('body').appendChild(p1);
+p1.id = "javaContent"
